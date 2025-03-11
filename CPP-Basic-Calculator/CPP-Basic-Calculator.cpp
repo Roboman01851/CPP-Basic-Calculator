a626c666c56b97a35255c7170bbbd58a102f34ca
Roboman01851 <roboman01851@gmail.com>
2025-03-10 21:39:55 -0400

    a

--- CPP-Basic-Calculator/CPP-Basic-Calculator.cpp
+++ CPP-Basic-Calculator/CPP-Basic-Calculator.cpp
@@ -1,9 +1,27 @@
-
+#include "SFML/Graphics.hpp"
 #include <iostream>
+
+const int Window_size_x = 500;
+const int Window_size_y = 800;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    sf::RenderWindow window(sf::VideoMode({ Window_size_x, Window_size_y}), "Basic C++ Calculator");
+	sf::RectangleShape background({Window_size_x, Window_size_y});
+	background.setFillColor(sf::Color(0x80, 0x80, 0x80));
+    
 
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+		window.draw(background);
+        window.display();
+    }
+}
 
